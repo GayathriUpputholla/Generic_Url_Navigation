6fbb76e049f63e83072f36622de4fbaccb03ef6a
unknown <you@example.com>
2023-06-01 16:25:27 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def srujana(request):
+    return render(request,'srujana.html')
+def sree(request):
+    return render(request,'sree.html')   
